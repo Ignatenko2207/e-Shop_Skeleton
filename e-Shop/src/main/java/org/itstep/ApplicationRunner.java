7ef297f0bbe2148eb593f14e956b8936af1bbcc6
Alex Ignatenko <ignatenko2207@gmail.com>
2018-04-16 20:54:59 +0300

    Fix order

--- e-Shop/src/main/java/org/itstep/ApplicationRunner.java
+++ e-Shop/src/main/java/org/itstep/ApplicationRunner.java
@@ -4,12 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-/**
- * @author Torii
- *
- *
- */
-
 @SpringBootApplication
 public class ApplicationRunner 
 {
