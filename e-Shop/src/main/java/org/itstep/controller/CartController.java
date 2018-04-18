259f8ee31f9c3b60257252531323fdd033d696eb
Alex Ignatenko <ignatenko2207@gmail.com>
2018-04-18 19:37:15 +0300

    Fix Cart controller

--- e-Shop/src/main/java/org/itstep/controller/CartController.java
+++ e-Shop/src/main/java/org/itstep/controller/CartController.java
@@ -1,5 +1,7 @@
 package org.itstep.controller;
 
+import java.util.List;
+
 import org.itstep.model.Cart;
 import org.itstep.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping (path = "/cart")
@@ -35,4 +38,30 @@ public class CartController {
 		}
 		return new ResponseEntity(HttpStatus.BAD_REQUEST);
 	}
+	
+	@DeleteMapping ( consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	ResponseEntity<Cart> deleteCart(@RequestBody Cart cart){
+		cartService.delete(cart);
+		return new ResponseEntity(HttpStatus.OK);
+	}
+	
+	@GetMapping (path = "/get-one", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	ResponseEntity<Cart> getCart(@RequestBody Integer id){
+		Cart cartFromDB = cartService.get(id);
+		if(cartFromDB != null) {
+			return new ResponseEntity<Cart>(cartFromDB, HttpStatus.OK);
+		}
+		return new ResponseEntity(HttpStatus.BAD_REQUEST);
+	}
+	
+	@GetMapping (path = "/get-list", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	ResponseEntity<List<Cart>> getCartsByPeriod(@RequestBody Long startPeriod, @RequestBody Long endPeriod, @RequestBody String login ){
+		List<Cart> cartsFromDB = cartService.findAllByCreationTime(startPeriod, endPeriod, login);
+		if(cartsFromDB != null) {
+			return new ResponseEntity<List<Cart>>(cartsFromDB, HttpStatus.OK);
+		}
+		return new ResponseEntity(HttpStatus.BAD_REQUEST);
+	}
+	
+	
 }
