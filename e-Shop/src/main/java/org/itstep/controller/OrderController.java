faa10767c3a52096697288f2355453d15bc5d6f6
Alex Ignatenko <ignatenko2207@gmail.com>
2018-04-18 19:39:53 +0300

    Fix Order controller

--- e-Shop/src/main/java/org/itstep/controller/OrderController.java
+++ e-Shop/src/main/java/org/itstep/controller/OrderController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,6 +38,21 @@ public class OrderController {
 			return new ResponseEntity<Order>(order, HttpStatus.OK);
 		}
 		
+		return new ResponseEntity(HttpStatus.BAD_REQUEST);
+	}
+	
+	@DeleteMapping ( consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	ResponseEntity<Order> deleteOrder(@RequestBody Order order){
+		orderService.delete(order);
+		return new ResponseEntity(HttpStatus.OK);
+	}
+	
+	@GetMapping ( produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	ResponseEntity<Order> getOrder(@RequestBody Integer id){
+		Order orderFromDB = orderService.get(id);
+		if(orderFromDB != null) {
+			return new ResponseEntity<Order>(orderFromDB, HttpStatus.OK);
+		}
 		return new ResponseEntity(HttpStatus.BAD_REQUEST);
 	}
 }
